88ff40d5c1636b219a4414eadbee7d8e96de9b26
rpavlov <ripavlovvv@gmail.com>
2026-09-12 04:08:09 +0500

    feat: adding new helper functions + logout

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,6 +2,10 @@ package auth
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"forum/internal/models"
 	"log"
@@ -34,19 +38,27 @@ func RegisterUser(ctx context.Context, conn *pgx.Conn, user models.RegisterReque
 
 }
 
-func AuthenticateUser(ctx context.Context, conn *pgx.Conn, user models.LoginRequest) bool {
+func AuthenticateUser(ctx context.Context, conn *pgx.Conn, user models.LoginRequest) (int, bool) {
 	query := `
-	SELECT password_hash
+	SELECT password_hash, id
 	FROM users
 	WHERE email = $1
 	`
-	hash, err := conn.Exec(ctx, query, user.Email)
+
+	var hash string
+	var userID int
+	err := conn.QueryRow(ctx, query, user.Email).Scan(&hash, &userID)
 	if err != nil {
 		log.Println(err)
-		return false
+		return 0, false
 	}
 
-	return VerifyPassword(user.Password, hash.String())
+	if !VerifyPassword(user.Password, hash) {
+		log.Println("Unauthorized")
+		return 0, false
+	}
+
+	return userID, true
 }
 
 func IsValidEmail(email string) bool {
@@ -72,3 +84,134 @@ func VerifyPassword(password string, hash string) bool {
 	}
 	return err == nil
 }
+
+func GenerateToken() (string, error) {
+	b := make([]byte, 32)
+
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.RawURLEncoding.EncodeToString(b), nil
+}
+
+func InsertSession(ctx context.Context, conn *pgx.Conn, userID int, access string, refresh string) error {
+	query := `
+	INSERT INTO sessions(user_id, access_token_hash, access_expires_at, refresh_token_hash, refresh_expires_at)
+	VALUES ($1, $2, $3, $4, $5)
+	`
+	_, err := conn.Exec(ctx, query, userID, access, time.Now().Add(15*time.Minute), refresh, time.Now().Add(14*24*time.Hour))
+	return err
+}
+
+func ValidateRefreshToken(ctx context.Context, conn *pgx.Conn, token string) (int, bool) {
+	query := `
+	SELECT user_id, refresh_expires_at
+	FROM sessions
+	WHERE refresh_token_hash = $1`
+	var refresh_expiration time.Time
+	var userID int
+	if err := conn.QueryRow(ctx, query, token).Scan(&userID, &refresh_expiration); err != nil {
+		log.Println(err)
+		return 0, false
+	}
+
+	if refresh_expiration.Before(time.Now()) {
+		return 0, false
+	}
+
+	return userID, true
+}
+
+func UpdateTokens(ctx context.Context, conn *pgx.Conn, userID int, access_hash string, new_refresh_token_hash string, old_refresh_token_hash string) bool {
+	query := `
+	UPDATE sessions
+	SET access_token_hash = $1,
+	access_expires_at = $2,
+	refresh_token_hash = $3,
+	refresh_expires_at = $4
+	WHERE user_id = $5 AND refresh_token_hash = $6
+	`
+
+	result, err := conn.Exec(ctx, query, access_hash, time.Now().Add(15*time.Minute), new_refresh_token_hash, time.Now().Add(14*24*time.Hour), userID, old_refresh_token_hash)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	if result.RowsAffected() == 0 {
+		log.Println("Cant find mathcing rows")
+		return false
+	}
+
+	return true
+}
+
+func HashToken(token string) string { //разобраться как работает
+	sum := sha256.Sum256([]byte(token))
+	return hex.EncodeToString(sum[:])
+}
+
+func StartSessionCleanup(conn *pgx.Conn) {
+	ticker := time.NewTicker(1 * time.Hour)
+	go func() {
+		for range ticker.C {
+			if _, err := conn.Exec(context.Background(), `DELETE FROM sessions WHERE refresh_expires_at < NOW()`); err != nil {
+				log.Println("Failed to delete session:", err)
+			}
+		}
+	}()
+}
+
+func DeleteSession(ctx context.Context, conn *pgx.Conn, refresh_hash string) error {
+	query := `
+	DELETE FROM sessions
+	WHERE refresh_token_hash = $1 `
+
+	result, err := conn.Exec(ctx, query, refresh_hash)
+
+	if err != nil {
+		return err
+	}
+
+	if result.RowsAffected() == 0 {
+		log.Println("No session matches")
+	}
+
+	return nil
+}
+
+func ValidateAccessToken(ctx context.Context, conn *pgx.Conn, access_hash string) (int, bool) {
+	query := `
+	SELECT user_id, access_expires_at
+	FROM sessions
+	WHERE access_token_hash = $1`
+	var userID int
+	var access_expiration time.Time
+	if err := conn.QueryRow(ctx, query, access_hash).Scan(&userID, &access_expiration); err != nil {
+		log.Println(err)
+		return 0, false
+	}
+
+	if access_expiration.Before(time.Now()) {
+		return 0, false
+	}
+
+	return userID, true
+}
+
+// func ValidateAccessToken(ctx context.Context, conn *pgx.Conn, access_hash string) (int, bool) {
+// 	query := `
+// 	SELECT user_id
+// 	FROM sessions
+// 	WHERE access_token_hash = $1 AND access_expires_at > $2
+// 	`
+// 	var userID int
+// 	if err := conn.QueryRow(ctx, query, access_hash, time.Now()).Scan(&userID); err != nil {
+// 		log.Println(err)
+// 		return 0, false
+// 	}
+
+// 	return userID, true
+// }
