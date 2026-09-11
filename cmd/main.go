eaaff1b86660b8bdec8a23d429dc68508288507a
rpavlov <ripavlovvv@gmail.com>
2026-09-12 04:09:33 +0500

    feat: melochi

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,53 @@
 package main
 
 import (
+	"context"
+	"forum/db/connection"
+	"forum/internal"
+	"forum/internal/auth"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	ctx := context.Background()
+
+	conn := connection.CreateConn(ctx)
+	defer conn.Close(ctx)
+
+	sql, err := os.ReadFile("db/migrations/001_create_users.up.sql")
+	if err != nil {
+		log.Fatal(err) //cant read file
+
+	}
+
+	_, err = conn.Exec(ctx, string(sql))
+	if err != nil {
+		log.Fatal(err) //cant run query
+	}
+
+	sql, err = os.ReadFile("db/migrations/002_create_auth_sessions.up.sql")
+	if err != nil {
+		log.Fatal(err) //cant read file
+
+	}
+
+	_, err = conn.Exec(ctx, string(sql))
+	if err != nil {
+		log.Fatal(err) //cant run query
+	}
+
+	handler := internal.NewHandler(conn)
+
+	auth.StartSessionCleanup(conn)
+
+	http.HandleFunc("/register", handler.HandleRegistration)
+	http.HandleFunc("/login", handler.HandleLogin)
+	http.HandleFunc("/logout", handler.Middleware(handler.HandleLogout))
+	http.HandleFunc("/", internal.MainHandler)
+	http.HandleFunc("/refresh", handler.HandleRefresh)
+
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
-module 01.tomorrow-school.ai/git/rpavlov/forum.git
+module forum
 
-go 1.25.7
+go 1.26.0
+
+require (
+	github.com/jackc/pgx/v5 v5.11.0
+	golang.org/x/crypto v0.57.0
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	golang.org/x/text v0.42.0 // indirect
+)
