a014b4b7a337c0963a42aedf6f4f7728aeb163cc
rpavlov <ripavlovvv@gmail.com>
2026-09-13 02:06:12 +0500

    feat: new models

--- internal/models/post.go
+++ internal/models/post.go
@@ -1,6 +1,17 @@
 package models
 
+import "time"
+
 type PostRequest struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
+}
+
+type Post struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
