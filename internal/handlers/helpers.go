3b740744d104fbae686b1a3a6ea8d97505f85ffd
rpavlov <ripavlovvv@gmail.com>
2026-09-13 02:07:54 +0500

    feat: new json helper

--- internal/handlers/helpers.go
+++ internal/handlers/helpers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -9,4 +10,13 @@ func writeJSONError(w http.ResponseWriter, message string, status int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(map[string]string{"message": message})
+}
+
+func writeJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Println(err)
+	}
 }
