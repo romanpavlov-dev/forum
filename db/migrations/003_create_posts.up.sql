0adff98fcb0aa9e596f6a5993c7045903d39ac6e
rpavlov <ripavlovvv@gmail.com>
2026-09-12 20:55:44 +0500

    feat: new tables

---
+++ db/migrations/003_create_posts.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS posts(
+    post_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL, 
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ
+)
