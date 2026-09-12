ad65fa20ac66391fb5b38ee6f11f8061ea70a097
rpavlov <ripavlovvv@gmail.com>
2026-09-13 02:05:49 +0500

    feat: new functionalities

--- internal/post_actions/post.go
+++ internal/post_actions/post.go
@@ -3,6 +3,7 @@ package post_actions
 import (
 	"context"
 	"forum/internal/models"
+	"log"
 	"time"
 
 	"github.com/jackc/pgx/v5"
@@ -22,9 +23,82 @@ func EditPost(ctx context.Context, conn *pgx.Conn, userID int, postID int, post 
 	query := `
 	UPDATE posts
 	SET title = $1, content = $2, updated_at = $3
-	WHERE user_id = $4 AND id = $5
+	WHERE user_id = $4 AND post_id = $5
 	`
 	_, err := conn.Exec(ctx, query, post.Title, post.Content, time.Now(), userID, postID)
 
 	return err
+}
+
+func DeletePost(ctx context.Context, conn *pgx.Conn, userID int, postID int) error {
+	query := `
+	DELETE FROM posts
+	WHERE userID = $1 AND postID = $2`
+
+	_, err := conn.Exec(ctx, query, userID, postID)
+
+	return err
+
+}
+
+func GetPost(ctx context.Context, conn *pgx.Conn, postID int) (models.Post, error) {
+	query := `
+	SELECT post_id, user_id, title, content, created_at
+	FROM posts
+	WHERE post_id = $1`
+
+	var post models.Post
+	if err := conn.QueryRow(ctx, query, postID).Scan(
+		&post.ID,
+		&post.UserID,
+		&post.Title,
+		&post.Content,
+		&post.CreatedAt,
+	); err != nil {
+		log.Println(err)
+		return models.Post{}, err
+	}
+
+	return post, nil
+}
+
+func GetAllPosts(ctx context.Context, conn *pgx.Conn) ([]models.Post, error) {
+	query := `
+	SELECT post_id, user_id, title, content, created_at
+	FROM posts
+	ORDER BY created_at DESC`
+
+	posts := make([]models.Post, 0)
+
+	rows, err := conn.Query(ctx, query)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var post models.Post
+		err := rows.Scan(
+			&post.ID,
+			&post.UserID,
+			&post.Title,
+			&post.Content,
+			&post.CreatedAt,
+		)
+		if err != nil {
+			log.Println(err)
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+
+	}
+
+	return posts, nil
 }
