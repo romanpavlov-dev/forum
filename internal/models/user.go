1706c54d3c376a25a7d64891f8a00f8b34996920
rpavlov <ripavlovvv@gmail.com>
2026-09-12 20:58:47 +0500

    feat: post model

--- internal/models/user.go
+++ internal/models/user.go
@@ -21,3 +21,8 @@ type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type PostRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
